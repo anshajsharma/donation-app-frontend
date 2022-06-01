b50ede54c52d85a2b70ae0ea978d0544d6eb069f
sonde vishnuvardhan rao <97151114+vishnusonde@users.noreply.github.com>
2022-06-01 23:36:05 +0530

    Add files via upload

---
+++ Footer/Footer.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import './footer.css'
+
+
+export default function Footer() {
+  return (
+    <footer class="footer">
+  	 <div class="container">
+  	 	<div class="row">
+  	 		<div class="footer-col">
+  	 			<h4>company</h4>
+  	 			<ul>
+  	 				<li><a href="#">about us</a></li>
+  	 				<li><a href="#">our services</a></li>
+  	 				<li><a href="#">privacy policy</a></li>
+  	 				<li><a href="#">affiliate program</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>get help</h4>
+  	 			<ul>
+  	 				<li><a href="#">FAQ</a></li>
+  	 				<li><a href="#">Help Centres</a></li>
+  	 				<li><a href="#">payment options</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>Donate</h4>
+  	 			<ul>
+  	 				<li><a href="#">Monthly</a></li>
+  	 				<li><a href="#">Yearly</a></li>
+  	 				<li><a href="#">connect with us</a></li>
+					   <li><a href="#">Newsletter</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>follow us</h4>
+  	 			<div class="social-links">
+					   <section id="footer" class="py-5">
+							<div class="container text-center">
+								<a href="#" class="twitter"></a>
+								<a href="#" class="linkedin"></a>
+								<a href="#" class="facebook"></a>
+							</div>
+                        </section>
+  	 			</div>
+  	 		</div>
+  	 	</div>
+  	 </div>
+  </footer>
+  )
+}
